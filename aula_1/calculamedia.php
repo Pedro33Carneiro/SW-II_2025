310290eebcec0ee3aff7bebe01b992ed8136cde3
Pedro33Carneiro <pdrcs1810@gmail.com>
2025-02-11 11:06:09 -0300

    calculanota

---
+++ aula_1/calculamedia.php
@@ -0,0 +1,11 @@
+<?php
+        $nota1 = 8
+        $nota2 = 6
+        $nota3 = 7
+
+            echo " Nota 1 =". $nota1 ."<br>";
+            echo " Nota 2 =". $nota2 ."<br>";
+            echo " Nota 3 =". $nota3 ."<br>";
+
+        
+?>
