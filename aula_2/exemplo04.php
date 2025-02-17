18479bd3bc5eb1dd2dae32d69c0691d7387b13e6
Pedr33Carneiro <pdrcs1810@gmail.com>
2025-02-17 11:06:11 -0300

    exercicios

---
+++ aula_2/exemplo04.php
@@ -0,0 +1,18 @@
+<?php
+    $nomes = ['Pedro','Rebeca', 'Aleks'];
+
+    $qtde = cont($nomes);
+
+    echo "Elementos: $qtde";
+
+    echo "<br>";
+
+for($i=0;$i<=$qtde;$i++) {
+    echo $nomes[1] 
+}
+
+
+    echo $nomes[1];
+
+    
+?>
---
+++ aula_2/exercicio01.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = 'Pedro';
+$idade = 17;
+$cidade = 'montemor';
+
+echo "meu nome é: $nome <br>";
+echo "tenho: $idade <br>";
+echo "sou de : $cidade <br>";
+
+?>
