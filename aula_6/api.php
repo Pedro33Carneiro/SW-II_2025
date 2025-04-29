0925bc4ab58282e7c2159374f61f9b1759c7f3ec
Pedro33Carneiro <pdrcs1810@gmail.com>
2025-04-29 11:28:37 -0300

    novaaula

---
+++ aula_6/api.php
@@ -0,0 +1,28 @@
+<?php
+    header("Content-Type: application/json");
+    $metodo = $_SERVER['REQUEST_METHOD'];
+    $usuarios = [
+        ["id" => 1, "nome" => "Pedro Santos", "email" => "pedro@email.com"],
+        ["id" => 2, "nome" => "Rebeca Leite", "email" => "rebeca@email.com"]
+    ];
+    switch ($metodo) {
+        case 'GET':
+            echo json_encode($usuarios);
+            break;        
+        case 'POST':
+            $dados = json_decode(file_get_contents('php://input'), true);
+            $novoUsuario = [
+                "id" => $dados["id"],
+                "nome" => $dados["nome"],
+                "email" => $dados["email"]
+            ];
+            array_push($usuarios, $novoUsuario);
+            echo json_encode("O usuário foi adicionado com sucesso.");
+            print_r($usuarios);
+            
+            break;        
+        default:
+            echo "O método requisitado não foi encontrado.";
+            break;        
+    }
+?>
